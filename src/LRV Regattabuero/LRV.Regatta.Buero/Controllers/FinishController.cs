3213f4c129ecbba0baab9a49333010d0685e8280
Stefan Petersen <stefan@openelp.de>
2024-09-11 13:26:49 +0200

    Added project and created finish controller

---
+++ src/LRV Regattabuero/LRV.Regatta.Buero/Controllers/FinishController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LRV.Regatta.Buero.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FinishController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Service Running";
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OnPostUploadAsync([FromForm] IFormFile file)
+        {
+
+                if (file.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+            return Ok();
+        }
+    }
+}
