7508c5b9190172f15e6371ac6341430168a2f624
Stefan Petersen <stefan@openelp.de>
2024-10-04 20:24:42 +0200

    updated finish controller

--- src/LRV.Regatta.Buero/Controllers/FinishController.cs
+++ src/LRV.Regatta.Buero/Controllers/FinishController.cs
@@ -35,8 +35,10 @@ namespace LRV.Regatta.Buero.Controllers
             {
                 // Create the directory
                 Directory.CreateDirectory(folderpath);
-                Console.WriteLine("Directory created successfully.");
+                Console.WriteLine($"\tDirectory {folderpath} created successfully.");
+                Console.WriteLine("");
             }
+
             if (file.Length > 0)
             {
                 FinishObject item = new FinishObject();
@@ -45,8 +47,10 @@ namespace LRV.Regatta.Buero.Controllers
                 item.Path =  file.FileName;
 
                 this._storageService.Add(item);
+                string filePath = Path.Combine(folderpath, file.FileName);
+                Console.WriteLine($"\tSaving file to {filePath}.");
 
-                using (var stream = System.IO.File.Create(Path.Combine(folderpath, file.FileName)))
+                using (var stream = System.IO.File.Create(filePath))
                 {
                     await file.CopyToAsync(stream);
                 }
