f133b9d2d3f3fb015d6575e8514f94f00a2279e6
Stefan Petersen <stefan@openelp.de>
2024-10-04 18:53:45 +0200

    Updated storage functionality

--- src/LRV.Regatta.Buero/Controllers/FinishController.cs
+++ src/LRV.Regatta.Buero/Controllers/FinishController.cs
@@ -13,11 +13,13 @@ namespace LRV.Regatta.Buero.Controllers
     {
         private IStorageService _storageService;
         private IConfiguration _configuration;
+        private string folderpath;
 
         public FinishController(IStorageService storage, IConfiguration config)
         {
             this._storageService = storage;   
             this._configuration = config;
+            this.folderpath = this._configuration.GetValue<string>("ImageFolder")
         }
 
         [HttpGet]
@@ -29,20 +31,26 @@ namespace LRV.Regatta.Buero.Controllers
         [HttpPost]
         public async Task<IActionResult> OnPostUploadAsync([FromForm] IFormFile file)
         {
-                if (file.Length > 0)
-                {
-                    FinishObject item = new FinishObject();
+            if (!Directory.Exists(folderpath))
+            {
+                // Create the directory
+                Directory.CreateDirectory(folderpath);
+                Console.WriteLine("Directory created successfully.");
+            }
+            if (file.Length > 0)
+            {
+                FinishObject item = new FinishObject();
 
-                    item.Name = file.FileName;
-                    item.Path =  file.FileName;
+                item.Name = file.FileName;
+                item.Path =  file.FileName;
 
                 this._storageService.Add(item);
 
-                    using (var stream = System.IO.File.Create(Path.Combine(this._configuration.GetValue<string>("ImageFolder"), file.FileName)))
-                    {
-                        await file.CopyToAsync(stream);
-                    }
+                using (var stream = System.IO.File.Create(Path.Combine(folderpath, file.FileName)))
+                {
+                    await file.CopyToAsync(stream);
                 }
+            }
 
             return Ok();
         }
