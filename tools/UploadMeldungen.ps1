e5184c943c7f5e3c52da48a7ead18a10de71aefe
Stefan Petersen <stefan@openelp.de>
2025-04-12 15:22:00 +0200

    updated dockerfile (fingerscrossed)
    Aktualisiere Projektkonfiguration und füge neue Dienste hinzu
    
    - Aktualisierung der `MinimumVisualStudioVersion` in der `LRV Regattabuero.sln`.
    - Hinzufügen von `README.md` und `UploadMeldungen.ps1` zu den Projektdateien.
    - Optimierung des Dockerfiles mit angepassten Arbeitsverzeichnissen und Kopieranweisungen.
    - Änderung des `EXPOSE`-Befehls auf Port 8080 in der Docker-Konfiguration.
    - Entfernen des Verweises auf `Microsoft.VisualStudio.Azure.Containers.Tools.Targets` in der `LRV.Regatta.Buero.csproj`.
    - Hinzufügen neuer Dienste (`regattadb`, `phpmyadmin`, `redis`, `redisInsights`) in der `docker-compose.yml`.
    - Erstellung eines PowerShell-Skripts (`UploadMeldungen.ps1`) zum Senden einer XML-Datei an eine API.

---
+++ tools/UploadMeldungen.ps1
@@ -0,0 +1,14 @@
+﻿# Path to the file you want to send
+$filePath = "C:\Users\stefa\Downloads\2025-04-08_20-30_drv-meldungen_95-lubeck-regatta.xml"
+
+# Read the file content
+$fileContent = Get-Content -Path $filePath -Raw
+
+# URL to which you want to send the request
+$url = "http://localhost:5015/api/Team"
+
+# Send the file content as the body of the POST request
+$response = Invoke-RestMethod -Uri $url -Method Post -Body $fileContent -ContentType "text/xml"
+
+# Output the response
+Write-Host $response
