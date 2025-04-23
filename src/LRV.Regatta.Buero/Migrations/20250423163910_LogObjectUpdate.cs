89a19dc5dada3a224efee8709f0ba307274d7217
Stefan Petersen <stefan@openelp.de>
2025-04-23 18:39:43 +0200

    Neue Spalten für LogObject hinzugefügt
    In der `DatabaseContextModelSnapshot.cs` wurden die erforderlichen Eigenschaften `ClientName` und `ClientVersion` zur `LogObject`-Entität hinzugefügt.
    
    Eine Migration in `20250423163910_LogObjectUpdate.cs` fügt diese Spalten zur Tabelle `LogObjects` hinzu und definiert sie als erforderlich.
    
    Das Modell in `20250423163910_LogObjectUpdate.Designer.cs` wurde aktualisiert, um die neuen Eigenschaften widerzuspiegeln und die Datenbankstruktur anzupassen.

---
+++ src/LRV.Regatta.Buero/Migrations/20250423163910_LogObjectUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LRV.Regatta.Buero.Migrations
+{
+    /// <inheritdoc />
+    public partial class LogObjectUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientName",
+                table: "LogObjects",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ClientVersion",
+                table: "LogObjects",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientName",
+                table: "LogObjects");
+
+            migrationBuilder.DropColumn(
+                name: "ClientVersion",
+                table: "LogObjects");
+        }
+    }
+}
